2546077040a1cd333ed138da1fead3c9a93119fb
KingDonggyu <dgkim69166916@gmail.com>
2023-10-14 16:38:17 +0900

    refactor: 초기 테마를 다크모드로 지정

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -4,5 +4,9 @@ import { ThemeProvider } from 'next-themes';
 import { ReactNode } from 'react';
 
 export function Providers({ children }: { children: ReactNode }) {
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+  return (
+    <ThemeProvider attribute="class" defaultTheme="dark">
+      {children}
+    </ThemeProvider>
+  );
 }
