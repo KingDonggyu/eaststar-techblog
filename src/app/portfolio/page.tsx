63e5c2039e11003bcad68ed531d1f24816d6e6cd
KingDonggyu <dgkim69166916@gmail.com>
2023-10-29 18:47:22 +0900

    refactor: portfolio 내 theme toggle 버튼 제거

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,7 +1,6 @@
 import 'styles/portfolio.css';
 import { Metadata } from 'next';
 import { PortfolioProject } from 'components/portfolio/projects';
-import { ThemeToggle } from 'components/theme-toggle';
 
 export const metadata: Metadata = {
   title: 'Portfolio - 개발자 동쪽별',
@@ -11,7 +10,6 @@ export const metadata: Metadata = {
 export default function Portfolio() {
   return (
     <div id="portfolio" className="px-28 py-[90px] max-w-[1440px] m-auto sm:px-[30px]">
-      <ThemeToggle className="fixed top-7 right-8" />
       <PortfolioProject />
     </div>
   );
